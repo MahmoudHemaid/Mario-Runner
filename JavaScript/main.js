3f4263a86cca9a1d7d21ce3b78d6c72061a04ef7
Mahmoud Hemaid <muui-1@hotmail.com>
2017-12-14 01:46:52 +0200

    Add files via upload
    After Add Bump

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -2,16 +2,18 @@ var screenWidth = document.getElementById("platform1").offsetWidth;
 var platform1 = document.getElementById("platform1");
 var platform2 = document.getElementById("platform2");
 var mountains = document.getElementById("mountains");
+var bump = document.getElementById("bump");
 var player = document.getElementById("player");
 console.log();
 (function () {
     platform1.style.left = - screenWidth + "px";
     platform2.style.left = "0px";
     mountains.style.left = - screenWidth + "px";
+    bump.style.left = - screenWidth + "px";
     player.style.bottom = "0vh";
 })();
 
-var platformMove = 15, mountainsMove = 8, jumpMove = 6;
+var platformMove = 15, mountainsMove = 6, jumpMove = 7, bumpMove = 30;
 
 console.log();
 var platform1Left = parseInt(platform1.style.left.slice(0, -2));
@@ -47,14 +49,24 @@ function playerRunning (){
         characterCase = 0;
     }
 }
+playerRunning();
 var mountainsLeft = parseInt(mountains.style.left.slice(0, -2));
+var bumpLeft = parseInt(bump.style.left.slice(0, -2));
 function mountainsFanc() {
-    if(mountainsLeft >= screenWidth * 2){
+    if(mountainsLeft >= screenWidth * 3){
         mountainsLeft = -screenWidth;
     }else{
         mountainsLeft +=  mountainsMove;
     }
     mountains.style.left = mountainsLeft + "px";
+}
+function bumpFanc() {
+    if(bumpLeft >= screenWidth * 3){
+        bumpLeft = -screenWidth;
+    }else{
+        bumpLeft +=  bumpMove;
+    }
+    bump.style.left = bumpLeft + "px";
 }
 var playerBottom = parseInt(player.style.bottom.slice(0, -2));
 var isJumping = false;
@@ -70,7 +82,7 @@ window.setInterval(function(){
     platform();
     playerRunning();
     mountainsFanc();
-    
+    bumpFanc();
     if (isJumping){
         if(jumpCount <= 6){
             playerBottom += jumpMove;
@@ -83,4 +95,5 @@ window.setInterval(function(){
         jumpCount++;
         player.style.bottom = playerBottom + "vh";
     }
+    console.log( "Player left" + player.offsetLeft + " bump left " + bump.offsetLeft);
 }, 60 );
